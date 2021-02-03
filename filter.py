f4092a25a33a6cfd82ed8acfd2b88e4987ae5df7
ThomasB123 <44202676+ThomasB123@users.noreply.github.com>
2021-02-03 19:00:39 +0000

    filtered dataset to restaurants only

--- filter.py
+++ filter.py
@@ -1,24 +1,69 @@
 
+import os
 import json
 import pandas as pd
 
 
-business_json_path = 'yelp_dataset/yelp_academic_dataset_business.json'
-'''
-totalCount = 0
-openCount = 0
-with open('yelp_dataset/yelp_academic_dataset_business.json','r') as f:
-    with open('processedData/business_filtered.json','w') as fout:
-        for line in f:
+def getIDs():
+    inFile = open('dataset/business.json','r')
+    outFile = open('processed/business_ids.json','w')
+    ids = {}
+    count = 0
+    for line in inFile:
+        business = json.loads(line)
+        ID = business['business_id']
+        try:
+            if business['is_open'] == 1 and 'Restaurant' in business['categories']:
+                ids[ID] = 1
+        except:
+            pass
+    json.dump(ids,outFile)
+    inFile.close()
+    outFile.close()
+    return ids
+
+def filterRelevant():
+    files = ['business.json','checkin.json','review.json','tip.json','covid_features.json']
+    for fileName in files:
+        inFile = open('dataset/'+fileName,'r')
+        outFile = open('processed/'+fileName,'w')
+        for line in inFile:
             business = json.loads(line)
-            isOpen = business['is_open']
-            if isOpen == 1:
-                openCount += 1
-                fout.write(line)
-            totalCount += 1
-print(openCount)
-print(totalCount)
+            if business['business_id'] in ids:
+                outFile.write(line)
+        inFile.close()
+        outFile.close()
+
+def toCSV():
+    files = ['business','checkin','review','tip','covid_features']
+    for fileName in files:
+        panda = pd.read_json('processed/'+fileName+'.json', lines=True)
+        panda.to_csv('processed/'+fileName+'.csv',index=False)
+
+#ids = getIDs()
+#filterRelevant()
+#toCSV()
+
+inFile = open('processed/covid_features.json')
+duplicates = ['D35unF350QG06LXDe0Cv-Q',
+'nLGgJBtMfGv7_juXipXheA',
+'b5uCtk5iUcdeSWBNAx40Ug',
+'l3joBBpkq0ib11dKUpKMAw',
+'4v0aj1VWtNwvzOeQ2Ohzrg',
+'BpL-s3p572cxNdXJAsahpA',
+'ZBfoUKOkfocsYK6TFQpwaA',
+'s2Fv9gzUnnpfnkUTAMSK7w']
+for line in inFile:
+    business = json.loads(line)
+    ID = business['business_id']
+    if ID in duplicates:
+        print(line)
+
+
+# user features: user_id, name, review_count, yelping_since, useful, funny, cool, elite, friends, fans, average_stars, compliment_hot, compliment_more, compliment_profile, compliment_cute, compliment_list, compliment_note, compliment_plain, compliment_cool, compliment_funny, compliment_writer, compliment_photos
+
 '''
+
 
 df_b = pd.read_json(business_json_path, lines=True)
 
@@ -42,8 +87,7 @@ business_restaurant.to_csv(csv_name, index=False)
 
 # filter by time
 # get number of reviews in last year?
-
-
+'''
 
 '''
 # filter dataset by city
